5ea6153a5820619d1a2b0063e567d10a83a8d838
16113-ftc <info@bothellrobotics.com>
2019-10-26 14:35:56 -0700

    anvi and dhriti

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpNavigation.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "TeleOpNavigation (Blocks to Java)", group = "")
+public class TeleOpNavigation extends LinearOpMode {
+
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() {
+        float speed;
+        double turnspeed;
+
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+
+        // Put initialization blocks here.
+        waitForStart();
+        if (opModeIsActive()) {
+            // Put run blocks here.
+            while (opModeIsActive()) {
+                while (!gamepad1.b) {
+                    speed = gamepad1.left_stick_y;
+                    leftFront.setPower(speed);
+                    leftBack.setPower(speed);
+                    rightFront.setPower(-speed);
+                    rightBack.setPower(-speed);
+                    turnspeed = -gamepad1.left_stick_x;
+                    leftFront.setPower(turnspeed / 1.5);
+                    leftBack.setPower(turnspeed / 1.5);
+                    rightFront.setPower(turnspeed / 1.5);
+                    rightBack.setPower(turnspeed / 1.5);
+                }
+                telemetry.update();
+            }
+        }
+    }
+}
